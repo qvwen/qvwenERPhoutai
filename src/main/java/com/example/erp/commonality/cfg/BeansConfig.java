feee0d91198dfb568aab892516b36761efbeef98
YouDuckBoom <youduckboom@qq.com>
2019-09-02 14:40:14 +0800

    s
    ss

--- src/main/java/com/example/erp/commonality/cfg/BeansConfig.java
+++ src/main/java/com/example/erp/commonality/cfg/BeansConfig.java
@@ -2,34 +2,30 @@ package com.example.erp.commonality.cfg;
 
  
 
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.Properties;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-import org.springframework.format.support.FormattingConversionServiceFactoryBean;
 
 import com.github.pagehelper.PageHelper;
 
  
 
 @Configuration
-@MapperScan(basePackages = { "com.example.erp.*.dao"})
+@MapperScan(basePackages = { "com.example.demo.dao" })
 public class BeansConfig {
 	
 	@Bean
-    public ConversionService conversionService() {
-        FormattingConversionServiceFactoryBean factory = new FormattingConversionServiceFactoryBean();
-        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
-        registrar.setDateFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        factory.setFormatterRegistrars(Collections.singleton(registrar));
-        factory.afterPropertiesSet();
-        return factory.getObject();
-    }
-
+	public PageHelper pageHelper() {
+		PageHelper pageHelper = new PageHelper();
+		Properties properties = new Properties();
+		properties.setProperty("offsetAsPageNum", "true");
+		properties.setProperty("rowBoundsWithCount", "true");
+		properties.setProperty("reasonable", "true");
+		properties.setProperty("dialect", "mysql");
+		pageHelper.setProperties(properties);
+		return pageHelper;
+	}
 
 }
