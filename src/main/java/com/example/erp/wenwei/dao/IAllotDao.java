51554981bd9a0e7a04d302f213e2d721be337340
YouDuckBoom <youduckboom@qq.com>
2019-09-02 14:28:21 +0800

    文唯提交

---
+++ src/main/java/com/example/erp/wenwei/dao/IAllotDao.java
@@ -0,0 +1,25 @@
+package com.example.erp.wenwei.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.erp.wenwei.pojo.DsproductprodidEntity;
+import com.example.erp.wenwei.vo.AllotVO;
+
+public interface IAllotDao {
+	
+	public List<AllotVO> findAllMove();
+	
+	public List<DsproductprodidEntity> findProdByWare(@Param("WareID") String WareID);
+
+	public int addMainMove(@Param("WareOutID") Integer wareoutid, @Param("WareInID") Integer wareinid,
+			@Param("Permitter") Integer permitter, @Param("MoveDate") String moveDate, @Param("MoveNO") String moveno,
+			@Param("MakerID") Integer makerid, @Param("MoveDetailNO") String movedetailno);
+
+	public int addDetailMove(@Param("MoveNO") String moveno, @Param("ProdID") String prodid,
+			@Param("Reason") String reason, @Param("MoveDetailNO") String movedetailno,
+			@Param("MoveBillNO") String movebillno);
+}
--- src/test/java/com/example/demo/ErpApplicationTests.java
+++ src/test/java/com/example/demo/ErpApplicationTests.java
@@ -1,16 +1,28 @@
 package com.example.demo;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.erp.wenwei.biz.AllotBiz;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes =ErpApplicationTests.class)
 public class ErpApplicationTests {
+	
+	@Autowired
+	private AllotBiz biz;
 
 	@Test
 	public void contextLoads() {
+		
 	}
 
 }
