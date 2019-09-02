ac531e367dc1bc365f333087f68681bb3be29116
YouDuckBoom <youduckboom@qq.com>
2019-09-02 14:47:59 +0800

    Revert "Revert "s""
    This reverts commit 0dd2f2f68116809a393a2ddb243c7cc1ebab34e2.

--- src/test/java/com/example/demo/test.java
+++ src/test/java/com/example/demo/test.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.demo;
 
 import javax.annotation.Resource;
@@ -24,3 +25,31 @@ public class test {
 		System.out.println(biz.addDetail("", "", "", "", ""));
 	}
 }
+=======
+package com.example.demo;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.erp.wenwei.biz.AllotBiz;
+ 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class test {
+	@Autowired
+	private AllotBiz biz;
+
+	@Test
+	public void contextLoads() {
+		System.out.println(biz.addDetail("", "", "", "", ""));
+	}
+}
+>>>>>>> parent of feee0d9... s
